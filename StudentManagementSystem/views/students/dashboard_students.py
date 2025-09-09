654813ccd75b14632bb4e1012bcb959c637e6c17
keentooyyy <kenneth.lobaton.58@gmail.com>
2025-09-09 12:40:27 +0800

    student dashboard design

--- StudentManagementSystem/views/students/dashboard_students.py
+++ StudentManagementSystem/views/students/dashboard_students.py
@@ -7,17 +7,23 @@ from StudentManagementSystem.models import Student
 def student_dashboard(request):
     student_id = request.session.get('user_id')
     if not student_id:
-        return redirect('unified_login')
+        return redirect('unified_logout')
 
     try:
         student = Student.objects.get(id=student_id)
+        first_name = student.first_name
+        last_name = student.last_name
+
+        full_name = f'{first_name} {last_name}'
     except Student.DoesNotExist:
-        return redirect('unified_login')
+        return redirect('unified_logout')
+
 
     performance = get_student_performance(student)
-    # print(performance)
-
-    return render(request, 'students/dashboard.html', {
+    context = {
         'student': student,
         'performance': performance,
-    })
+        'username': full_name,
+        'role': student.role,
+    }
+    return render(request, 'students/dashboard.html',context )
